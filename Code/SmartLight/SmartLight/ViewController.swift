0de6bbc0577c4861e98c986cf161a1ae80cf7202
liuxianghong <178483623@qq.com>
2017-04-10 14:58:44 +0800

    no message

---
+++ Code/SmartLight/SmartLight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartLight
+//
+//  Created by 刘向宏 on 2017/4/10.
+//  Copyright © 2017年 刘向宏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
